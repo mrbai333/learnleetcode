b2a0386cdc160065b749c5e5c11621b785cd66ee
mrbai <ricbai1986@icloud.com>
2019-06-24 21:30:22 +0800

    LeetCode 169 dict.items()

--- 169. Majority Element.py
+++ 169. Majority Element.py
@@ -2,6 +2,7 @@
 # 使用枚举的方式进行判断
 # 1.首先建立字典，枚举列表中的元素，进行计数，将元素和元素的个数作为键值对存放在字典中
 # 2.判断字典中的value值，如果有大于列表一半长度的则返回该value值对应的key值
+# 3.切记python 3中字典的遍历要使用dict.items()，不要使用enumerate，因为enumerate遍历出来的是key值和key值在dict中的位置
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
         numdict = {}
